175176e8f4b7b343d69445e88e566e0bab6ab181
Aishwarya Ramanathan <cutieice@gmail.com>
2020-03-21 14:22:06 -0400

    updated page with lecture and submit links

--- scripts.js
+++ scripts.js
@@ -30,10 +30,11 @@ var config = {
     links: [
         {key: "marks", url: "https://docs.google.com/spreadsheets/d/1xVwISSWXJJKRenIa2uEx2CcAih7aZF1dAVuTif9UvTs/edit?usp=sharing"},
         {key: "889", url: "https://docs.google.com/document/d/1_NSffitWxfZMYxGgjmwFhHT5aFVwRnxd7OSQsPquZLA/edit"},
+        {key: "889l", url: "https://uwaterloo.webex.com/meet/jianzhao"},
+        {key: "889d", url: "https://drive.google.com/drive/u/1/folders/1uZfotSR2LxOjjiTqKQk-EMom0Swnk0OX"},
         {key: "848", url: "https://cormack.uwaterloo.ca/cs848/"},
         {key: "848s", url: "https://cormack.uwaterloo.ca/submit/"},
-        {key: "res", url: "https://cormack.uwaterloo.ca/cs848/results/"},
-        {key: "cal", url: "https://cormack.uwaterloo.ca/cal/"},
+        {key: "848l", url: "https://georgetown.zoom.us/j/2128618097"},
         {key: "492", url: "https://www.student.cs.uwaterloo.ca/~cs492/"}
  ],
     dock: [
