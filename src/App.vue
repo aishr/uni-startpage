a70964e9e05d8b72cd1eb2f8944ad792845b818e
Aishwarya Ramanathan <aishr@google.com>
2021-10-22 11:54:16 -0400

    Started porting to vuejs

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HomeVue from './components/Home.vue';
+</script>
+
+<template>
+  <HomeVue/>
+</template>
+
+<style lang="scss">
+  html {
+    background: no-repeat center center fixed;
+    background-size: cover;
+    background-repeat: repeat-y;
+    background-color: #000000;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+function getRandomInt(max:number) {
+    return (Math.floor(Math.random() * Math.floor(max))) + 1;
+}
+let maxBackground:number = 50;
+let --image: string = "url(./src/assets/backgrounds/cat" + getRandomInt(maxBackground) + ".jpg)";
+</script>
+
+<template>
+  <div class="marks">
+    <a href="https://docs.google.com/spreadsheets/d/1xVwISSWXJJKRenIa2uEx2CcAih7aZF1dAVuTif9UvTs/edit?usp=sharing">
+      <img className="troll" title="Marks" src="./../assets/trollface.png" alt="marks"/>
+    </a>
+  </div>
+</template>
+
+<style lang="scss">
+.troll {
+  width: 20px;
+  height: 20px;
+}
+.marks {
+  position: absolute;
+  bottom: 0%;
+  left: 0%;
+}
+html {
+  background-image:;
+	background: no-repeat center center fixed;
+	background-size: cover;
+	background-repeat: repeat-y;
+	background-color: #000000;
+}
+</style>
