6e908527e35112866a67d61821fe304ad369b6fd
Aishwarya Ramanathan <cutieice@gmail.com>
2022-01-14 17:00:29 -0500

    changed import of troll face

--- src/components/Marks.vue
+++ src/components/Marks.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import troll from '/assets/trollface.png';
 </script>
 
 <template>
   <div class="marks">
     <a href="https://docs.google.com/spreadsheets/d/1xVwISSWXJJKRenIa2uEx2CcAih7aZF1dAVuTif9UvTs/edit?usp=sharing">
-      <img className="troll" title="Marks" src="/assets/trollface.png" alt="marks"/>
+      <img className="troll" title="Marks" :src=troll alt="marks"/>
     </a>
   </div>
 </template>
